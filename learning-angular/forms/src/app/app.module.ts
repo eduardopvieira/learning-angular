62fc01003e2cf7af24d898f7404bd9c053912382
eduardopvieira <eduardoopazvieira@gmail.com>
2024-08-27 16:20:30 -0300

    mudanças

--- learning-angular/forms/src/app/app.module.ts
+++ learning-angular/forms/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
 
 
@@ -13,12 +11,11 @@ import { TemplateFormModule } from './template-form/template-form.module';
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
+    //ReactiveFormsModule,
     BrowserAnimationsModule,
     TemplateFormModule
   ],
---
+++ learning-angular/forms/src/app/data-form/data-form.module.ts
@@ -0,0 +1,15 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataFormComponent } from './data-form.component';
+
+
+
+@NgModule({
+  declarations: [DataFormComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class DataFormModule { }
