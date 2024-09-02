1366b81f64b167605e480f5941b5460eab528028
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-02 14:03:15 -0300

    feat: Logica da confirmação de email e da validação de cep

--- learning-angular/forms/src/app/data-form/data-form.component.ts
+++ learning-angular/forms/src/app/data-form/data-form.component.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
 import { DropdownService } from '../shared/services/dropdown.service';
 import { EstadoBr } from '../shared/models/estado-br';
 import { ConsultaCepService } from '../shared/services/consulta-cep.service';
-import { Observable } from 'rxjs';
+import { FormValidations } from '../shared/form-validations';
 
 @Component({
   selector: 'app-data-form',
@@ -19,6 +21,8 @@ export class DataFormComponent {
   cargos: any[] = [];
   tecnologias: any[] = [];
   newsletterOp: any[] = [];
+
+  frameworks = ['Angular', 'React', 'Vue', 'Sencha'];
 
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private dropdownServ: DropdownService, private cepService: ConsultaCepService) { }
@@ -53,6 +57,17 @@ export class DataFormComponent {
     }
   }
 
+  buildFrameworks() {
+
+    const values = this.frameworks.map(v => new FormControl(false));
+
+    return this.formBuilder.array(values, FormValidations.requiredMinCheckBox(1));
+  }
+
+  frameworksArrayControls() {
+    return (this.formulario.get('frameworks') as FormArray).controls;
+  }
+
 
   populaDadosForm(dados: any) {
 
@@ -85,7 +100,16 @@ export class DataFormComponent {
 
   onSubmit() {
 
-    console.log(this.formulario.value);
+
+    let valueSubmit = Object.assign({}, this.formulario.value); //cria uma copia do formulario para nao afetar o original
+
+    valueSubmit = Object.assign(valueSubmit, {
+      frameworks: valueSubmit.frameworks
+        .map((v: boolean, i: number) => v ? this.frameworks[i] : null)
+        .filter((v: string | null) => v !== null)
+    })
+
+    console.log(valueSubmit);
 
     if (this.formulario.valid) {
 
@@ -114,7 +138,6 @@ export class DataFormComponent {
 
   compararCargos(obj1: any, obj2: any) {
     return obj1 && obj2 ? (obj1.nome === obj2.nome && obj1.nivel === obj2.nivel) : obj1 === obj2
-
   }
 
   ngOnInit() {
@@ -129,8 +152,9 @@ export class DataFormComponent {
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
       email: [null, [Validators.required, Validators.email]],
+      confirmarEmail: [null, [Validators.required, Validators.email, FormValidations.equalsTo('email')]],
       endereco: this.formBuilder.group({
-        cep: [null, [Validators.required]],
+        cep: [null, [Validators.required, FormValidations.cepValidator]],
         numero: [null, [Validators.required]],
         complemento: [null],
         rua: [null, [Validators.required]],
@@ -141,7 +165,9 @@ export class DataFormComponent {
 
       cargo: [null],
       tecnologias: [null],
-      newsletter: ['s']
+      newsletter: ['s'],
+      termos: [null, [Validators.requiredTrue]],
+      frameworks: this.buildFrameworks()
     });
 
   };
