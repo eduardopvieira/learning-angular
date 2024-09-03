d36123996dbf6e3ec077624cadb700ce9f1febd9
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-03 15:09:22 -0300

    refactor: Adição dos componentes de mensagem de erro, campo de input e FormsModule

--- learning-angular/forms/src/app/shared/shared.module.ts
+++ learning-angular/forms/src/app/shared/shared.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormDebugComponent } from './form-debug/form-debug.component';
+import { ErrorMsgComponent } from './error-msg/error-msg.component';
+import { InputFieldComponent } from './input-field/input-field.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     FormDebugComponent,
+    ErrorMsgComponent,
+    InputFieldComponent,
 
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
-    FormDebugComponent
+    FormDebugComponent,
+    ErrorMsgComponent,
+    InputFieldComponent
   ]
 })
 export class SharedModule { }
