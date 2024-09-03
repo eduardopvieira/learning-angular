d54ef9d2a950f507c47aa6baa0dfb593ab42b028
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-03 15:08:41 -0300

    refactor: Remoção do componente de controle de erro

--- learning-angular/forms/src/app/app.module.ts
+++ learning-angular/forms/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormModule } from './template-form/template-form.module';
 import { DataFormModule } from './data-form/data-form.module';
-import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
 
 
 
