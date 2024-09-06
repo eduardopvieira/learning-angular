19fa5a413cae4ca4772481724b43a737cb261a36
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-06 16:56:57 -0300

    feat: Criação do service do componente cursos

---
+++ learning-angular/requests-http/src/app/cursos/cursos.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Curso } from './cursos-lista/curso';
+import { delay, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = 'http://localhost:3000/cursos';
+
+  constructor(private http: HttpClient) { }
+
+  list() {
+    return this.http.get<Curso[]>(this.API)
+      .pipe(
+        delay(2000),
+        tap(console.log)
+      );
+  }
+
+} 
