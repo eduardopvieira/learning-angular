92ffc569c36c4f21dd6780f3a81f3f8a04cc7db8
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-06 16:57:58 -0300

    feat: Componente principal da lista de cursos

---
+++ learning-angular/requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from '../cursos.service';
+import { Curso } from './curso';
+import { Observable, subscribeOn } from 'rxjs';
+
+@Component({
+  selector: 'app-cursos-lista',
+  templateUrl: './cursos-lista.component.html',
+  styleUrl: './cursos-lista.component.scss',
+  preserveWhitespaces: true
+})
+export class CursosListaComponent implements OnInit {
+
+  cursos: Curso[] = [];
+
+  cursos$!: Observable<Curso[]>; //notação finlandesa: variaveis terminadas em $ indicam observables
+
+  constructor(private service: CursosService) { }
+
+  ngOnInit() {
+    // this.service.list()
+    //   .subscribe(dados => this.cursos = dados);
+
+    this.cursos$ = this.service.list();
+
+
+  }
+
+}
