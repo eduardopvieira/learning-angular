ec4b429fc157f2dcf6318983ba084406e7f2e5d1
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-06 16:55:29 -0300

    feat: Criação do modulo de rotas da aplicação

--- learning-angular/requests-http/src/app/app-routing.module.ts
+++ learning-angular/requests-http/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'cursos'
+  },
+  // ! rota de lazy loading:
+  {
+    path: 'cursos',
+
+    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
