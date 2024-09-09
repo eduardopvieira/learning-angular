b627b2df2aaf34cc655c7d97ff5aa7041a79c86e
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-09 15:17:40 -0300

    feat: Módulo de rotas do unsubscribe rxjs

---
+++ learning-angular/requests-http/src/app/unsubscribe-rxjs/unsubscribe-rxjs-routing.module.ts
@@ -0,0 +1,13 @@
+import { UnsubscribePocComponent } from './unsubscribe-poc/unsubscribe-poc.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', component: UnsubscribePocComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class UnsubscribeRxJsRoutingModule { }
