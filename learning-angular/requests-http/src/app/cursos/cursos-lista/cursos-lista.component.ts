b2d1dbe464577175b46e6e5bb28e1c41b6fe3308
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-12 19:47:32 -0300

    feat: Implementação "onDelete()"

--- learning-angular/requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ learning-angular/requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from './curso';
-import { catchError, empty, Observable, of, Subject, subscribeOn } from 'rxjs';
+import { catchError, EMPTY, empty, Observable, of, Subject, subscribeOn, switchMap, take } from 'rxjs';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { AlertModalComponent } from '../../shared/alert-modal/alert-modal.component';
 import { AlertModalService } from '../../shared/alert-modal.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Cursos2Service } from '../cursos2.service';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -15,15 +16,19 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CursosListaComponent implements OnInit {
 
-  //cursos: Curso[] = [];
-
   bsModalRef: BsModalRef = new BsModalRef();
 
   cursos$!: Observable<Curso[]>; //notação finlandesa: variaveis terminadas em $ indicam observables
   error$ = new Subject<boolean>();
 
+  idCursoSelecionado: string = '';
 
-  constructor(private service: CursosService,
+  deleteModalRef: BsModalRef = new BsModalRef();
+  @ViewChild('deleteModal') deleteModal!: TemplateRef<any>;
+
+  constructor(
+    private service: Cursos2Service,
+    private modalService: BsModalService,
     private alertService: AlertModalService,
     private router: Router,
     private route: ActivatedRoute) {
@@ -58,6 +63,38 @@ export class CursosListaComponent implements OnInit {
     } else {
       console.log("id é nulo deu erro aqui (cursos-lista-component.ts")
     }
+  }
+
+  onDelete(id: string) {
+    this.idCursoSelecionado = id;
+    //this.deleteModalRef = this.modalService.show(this.deleteModal, { class: 'modal-sm' });
+    const result$ = this.alertService.showConfirm('Confirmar', 'Tem certeza que deseja remover esse curso?')
+    result$.asObservable().pipe(
+      take(1),
+      switchMap(result => result ? this.service.remove(id) : EMPTY)
+    ).subscribe(
+      success => {
+        this.onRefresh();
+        this.deleteModalRef.hide();
+
+      },
+      error => this.alertService.showAlertDanger("Erro ao remover curso. Tente novamente mais tarde.")
+    );
+  }
+
+  onConfirmDelete() {
+    this.service.remove(this.idCursoSelecionado).subscribe(
+      success => {
+        this.onRefresh();
+      },
+      error => this.alertService.showAlertDanger("Erro ao remover curso. Tente novamente mais tarde.")
+    );
+    //nao precisa fazer unsubscribe pois no metodo de remove tem
+    //o take 1, que cancela a inscriçao sozinho
+  }
+
+  onDeclineDelete() {
+    this.deleteModalRef.hide();
   }
 
 }
