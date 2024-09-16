af0f834f053f28c702332b5e92ba7c0974176e14
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-16 17:08:33 -0300

    feat: Criação das funções de download

--- learning-angular/requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ learning-angular/requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UploadFileService } from '../upload-file.service';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { filterResponse, uploadProgress } from '../../shared/rxjs-operators';
 
 @Component({
   selector: 'app-upload-file',
@@ -38,20 +39,43 @@ export class UploadFileComponent {
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.service.upload(this.files, '/api/upload')
-        .subscribe((event: HttpEvent<Object>) => {
-          // HttpEventType
-          console.log(event);
-          if (event.type === HttpEventType.Response) {
-            console.log('Upload Concluído');
-          } else if (event.type === HttpEventType.UploadProgress) {
-            if (event.total) {
-              const percentDone = Math.round((event.loaded * 100) / event.total);
-              this.progress = percentDone;
-              console.log('Progresso', percentDone);
-            }
-          }
-        });
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload concluido'))
+
+      // .subscribe((event: HttpEvent<Object>) => {
+      //console.log(event);
+      //   if (event.type === HttpEventType.Response) {
+      //console.log('Upload Concluído');
+      //   } else if (event.type === HttpEventType.UploadProgress) {
+      //     if (event.total) {
+      //       const percentDone = Math.round((event.loaded * 100) / event.total);
+      //       this.progress = percentDone;
+      //console.log('Progresso', percentDone);
+      //     }
+      //   }
+      // });
     }
+  }
+
+  onDownloadExcel() {
+    this.service.download('/api/downloadExcel')
+      .subscribe((res: any) => {
+        this.service.handleFile(res, 'testeExcel.xlsx')
+      })
+  }
+
+  onDownloadPDF() {
+    this.service.download('/api/downloadPDF')
+      .subscribe((res: any) => {
+        this.service.handleFile(res, 'testePDF.pdf'),
+          console.log('mas nao chego aq')
+      })
   }
 
 }
